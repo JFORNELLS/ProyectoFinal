02dd0db2e2616e71db20a0f2c2663b751ff9fb95
JFORNELLS <jfornells@yahoo.es>
2023-07-21 12:47:34 +0200

    Creació debtToken i tests aToken debtToken

--- test/aTokenTest.t.sol
+++ test/aTokenTest.t.sol
@@ -6,5 +6,31 @@ import "../src/aToken.sol";
 
 contract aTokenTest is Test {
 
+    aToken public atoken;
+    address public alice;
+
+    function setUp() public {
+        atoken = new aToken();
+        alice = makeAddr("alice");
+    }
+
+    function testMintAtiken() public {
+        uint256 supply = atoken.totalSupply();
+        atoken.mintAToken(alice, 10 ether);
+        assertEq(atoken.balanceOf(alice), 10 ether);
+        assertEq(atoken.totalSupply(), supply + 10 ether);
+    }
+
+    function testBurnDebToken() public {
+        atoken.mintAToken(alice, 10 ether);
+        assertEq(atoken.balanceOf(alice), 10 ether);
+        uint256 supply = atoken.totalSupply();
+        atoken.burnAToken(alice, 5 ether);
+        assertEq(atoken.balanceOf(alice), 5 ether);
+        assertEq(atoken.totalSupply(), supply - 5 ether);
+    }
+
+
+
 }
     
---
+++ test/debtToken.t.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/debtToken.sol";
+
+contract debtTokenTest is Test {
+
+    debtToken public debt;
+    address public alice;
+
+    function setUp() public {
+        debt = new debtToken();
+        alice = makeAddr("alice");
+    }
+
+    function testMintDebtToken() public {
+        uint256 supply = debt.totalSupply();
+        debt.mintDebtToken(alice, 10 ether);
+        assertEq(debt.balanceOf(alice), 10 ether);
+        assertEq(debt.totalSupply(), supply + 10 ether);
+    }
+
+    function testBurndDebtToken() public {
+        debt.mintDebtToken(alice, 10 ether);
+        assertEq(debt.balanceOf(alice), 10 ether);
+        uint256 supply = debt.totalSupply();
+        debt.burnDebtToken(alice, 5 ether);
+        assertEq(debt.balanceOf(alice), 5 ether);
+        assertEq(debt.totalSupply(), supply - 5 ether);
+    }
+
+
+
+}
+    
