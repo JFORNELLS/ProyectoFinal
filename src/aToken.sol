5df73b932db4e3b52d1fa390159f942813b0a817
JFORNELLS <jfornells@yahoo.es>
2023-08-24 20:47:25 +0200

    ttt

--- src/aToken.sol
+++ src/aToken.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.20;
 import "../lib/solmate/src/tokens/ERC20.sol";
 contract AToken is ERC20 {
 
-
     constructor() ERC20("AToken", "ATN", 18) {}
+
 
     function mintAToken(address to, uint256 amount) public {
         _mint(to, amount);
